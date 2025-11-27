e69a3761871bbf2a589651543d431f328e822442
aruptime-ai <aruptime@gmail.com>
2025-11-27 14:49:31 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "hitesh"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
