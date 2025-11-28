e4cbe69a38bbbd8c174ca36f44f41712ca6bacbb
aruptime-ai <aruptime@gmail.com>
2025-11-28 03:51:38 +0000

    new file added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,24 +1,38 @@
 let score = "hitesh"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
 
 // console.log(valueInNumber)
 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
+// let booleanisLoggedIn = Boolean(isLoggedIn)
 
 // console.log(booleanisLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+// -----------------------------------------------------------------operations-------------------------------------------------------------
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2**6);
+//console.log(2/3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
