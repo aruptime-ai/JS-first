90c5d8316e481332125a97be1be1f7250989dccf
aruptime-ai <aruptime@gmail.com>
2025-11-28 04:26:56 +0000

    datatypes added

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,31 @@
+// primitive data types
+
+// 7 types : String, Number, Booblean, Null, Undefinded, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log( id === anotherId );
+
+const bigNumber = 26363474859596968674848443n
+//console.log(typeof bigNumber);
+
+// Reference ( Non Primitive ) data types
+
+// Array, Objects, Functions
+
+// Array example- array badi bracet me hota hai
+
+const heros = ["shaktimaan", "naagraj", "doga" ]
+
+// objects - object data type { madhyam bracet } me likha jata hai, aur = ke badle : likha jata hai value batane ke liye, shayad. isko variable me bhi store kar sakte hain
+
+let myobj = {
+    name: "hitesh",
+    age: 22,
+
+}
+
+//console.log(heros)
+
+
+// Functions 
